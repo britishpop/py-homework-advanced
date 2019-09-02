427850cfa402b2b0f2924c3eebc40ed0aff1f98f
britishpop <drmakc@gmail.com>
2019-09-02 07:39:08 +0300

    diploma fixes

--- 3.1.Diploma/test_search.py
+++ 3.1.Diploma/test_search.py
@@ -1,23 +1,34 @@
-from functionality import saver
+import json
+import os
 import unittest
+from functionality import saver
 
-from fixtures.user_data import expect_user
-from fixtures.user_data import criteria
-from fixtures.user_data import filtered_data
-from fixtures.user_data import raw_data
 
 class TestFiltering(unittest.TestCase):
 
+    def setUp(self):
+        current_path = str(os.path.dirname(os.path.abspath(__file__)))
+        f_expect_user = os.path.join(current_path, 'fixtures/expect_user.json')
+        f_criteria = os.path.join(current_path, 'fixtures/criteria.json')
+        f_users_data = os.path.join(current_path, 'fixtures/users_data.json')
+        with open(f_expect_user, "r") as f:
+            self.expect_user = json.load(f)
+        with open(f_criteria, "r") as f:
+            self.criteria = json.load(f)
+        with open(f_users_data, "r") as f:
+            users_data = json.load(f)
+            self.filtered_data = users_data["filtered_data"]
+            self.raw_data = users_data["raw_data"]
+
     def test_return_right_score(self):
-        result = saver.get_score_for_user(expect_user, criteria)
+        result = saver.get_score_for_user(self.expect_user, self.criteria)
         self.assertEqual(result, 103)
 
-
     def test_filtering_users(self):
-        result = saver.filter_for_users(raw_data, criteria)
-        self.assertEqual(result, filtered_data)
+        result = saver.filter_for_users(self.raw_data, self.criteria)
+        self.assertEqual(result, self.filtered_data)
 
 
 if __name__ == '__main__':
-    suite_1 = unittest.TestLoader().loadTestsFromTestCase(TestFiltering)
-    unittest.TextTestRunner(verbosity=2).run(suite_1)
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestFiltering)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
--- 3.1.Diploma/test_user.py
+++ 3.1.Diploma/test_user.py
@@ -1,25 +1,54 @@
-from classes import vk_users
+import json
+import os
 import unittest
-from mock import patch
-from fixtures.user_data import raw_user
-from fixtures.user_data import expect_user
+from mock import patch, Mock
+from classes import config
+from classes import vk_users
 
 
 class TestUser(unittest.TestCase):
+    maxDiff = None
 
+    def setUp(self):
+        current_path = str(os.path.dirname(os.path.abspath(__file__)))
+        f_expect_user = os.path.join(current_path, 'fixtures/expect_user.json')
+        f_raw_user = os.path.join(current_path, 'fixtures/raw_user.json')
+        f_expect_user_for_config = os.path.join(current_path,
+                                                'fixtures/expect_user_for_config.json')
+        f_criteria = os.path.join(current_path, 'fixtures/criteria.json')
+        with open(f_expect_user, "r") as f:
+            self.expect_user = json.load(f)
+        with open(f_raw_user, "r") as f:
+            self.raw_user = json.load(f)
+        with open(f_expect_user_for_config, "r") as f:
+            self.expect_user_for_config = json.load(f)
+        with open(f_criteria, "r") as f:
+            self.criteria = json.load(f)
 
-    @patch('classes.vk_users.MainUser.data', return_value=raw_user) #говорит, что нет такого аттрибута
+    @patch('classes.vk_users.BaseUser', autospec=True)
     def test_return_right_user_data(self, f):
-        result = self.obj.get_user_data()
-        self.assertEqual(result, expect_user)
+        u = vk_users.MainUser(1, 2)
+        u.data = self.raw_user
+        result = u.get_user_data()
+        self.assertEqual(result, self.expect_user)
 
-
-    # def test_return_right_photos(self):
-    #     data = [{'id': 288668576, 'likes': 1459882, 'url': 'https://pp.userapi.com/c7003/v7003978/1edb/S9N9m1NFFx4.jpg'}, {'id': 263219656, 'likes': 972081, 'url': 'https://pp.userapi.com/c9591/u00001/136592355/x_dbfafe4c.jpg'}, {'id': 263219735, 'likes': 868439, 'url': 'https://pp.userapi.com/c9591/u00001/136592355/x_d51dbfac.jpg'}]
-    #     result = self.obj_2.get_top_3_photos()
-    #     self.assertEqual(len(result), len(data))
-
+    @patch('classes.vk_users.BaseUser', autospec=True)
+    @patch('classes.config.BaseConfig', autospec=True)
+    def test_return_full_data(self, f_1, f_2):
+        mock = Mock()
+        u = vk_users.MainUser(1, 2)
+        u.data = self.expect_user
+        u.fields = self.criteria
+        mock.get_sex_for_search.return_value = 2
+        mock.get_age_for_search.return_value = (28, 32)
+        mock.get_music_for_search.return_value = ''
+        mock.get_book_for_search.return_value = "Достоевский"
+        mock.get_movie_for_search.return_value = "12 Angry Men"
+        mock.get_interest_for_search.return_value = ''
+        u.config = mock
+        result = u.get_data_for_search()
+        self.assertEqual(result, self.expect_user_for_config)
 
 if __name__ == '__main__':
-    suite_1 = unittest.TestLoader().loadTestsFromTestCase(TestUser)
-    unittest.TextTestRunner(verbosity=2).run(suite_1)
+    SUITE = unittest.TestLoader().loadTestsFromTestCase(TestUser)
+    unittest.TextTestRunner(verbosity=2).run(SUITE)
